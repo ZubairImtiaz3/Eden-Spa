14be87300961499a87911d34b7c604004a50f98d
Zubair <zubairimtiaz395@gmail.com>
2022-10-30 17:11:17 +0500

    Nextjs+Tailwind Basic Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
